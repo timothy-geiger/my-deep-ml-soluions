d4e721a79aeed6c5d2d838af7a4282ee6f124111
timothy-geiger <timothy.geiger@gmx.de>
2024-11-10 12:50:41 +0100

    completed 36

---
+++ solutions/36-calculate-accuracy-score.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+def accuracy_score(y_true, y_pred):
+    return np.sum(y_true == y_pred, axis=0) / len(y_true)
+
+
+y_true = np.array([1, 0, 1, 1, 0, 1])
+y_pred = np.array([1, 0, 0, 1, 0, 1])
+
+res = accuracy_score(y_true, y_pred)
+print(res)
